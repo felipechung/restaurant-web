4daabf5c14da3f2eb489cd65a348e1643dd1e6e5
Felipe <lipechung@hotmail.com>
2020-08-16 18:20:01 -0500

    Remove Animatecss, add animations on styles.css

---
+++ public/contact-script.js
@@ -0,0 +1,29 @@
+const buttonSend = document.getElementById("send-message");
+buttonSend.addEventListener("click", ()=> {
+
+    const nome = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;    
+    
+    if(nome == "" || email == "" || message == ""){
+        swal({
+            title: "Ops!",
+            text: "Verifique os campos novamente",
+            icon: "error",
+            buttons: {
+                confirm : {text:'OK',className:'sweet-confirm'},                
+            },
+        })
+    } else {
+        swal({
+            title: "Obrigado!",
+            text: "Mensagem enviada com sucesso!",
+            icon: "success",
+            buttons: {
+                confirm : {text:'OK',className:'sweet-confirm'},                
+            },
+        }).then(function() {
+            window.location = "http://localhost:3000/contact";
+        });
+    }
+})
