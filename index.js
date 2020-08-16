0eaaf7d27dc5dae46625f8d92bc543c1351da3d4
Felipe <lipechung@hotmail.com>
2020-08-16 17:00:48 -0500

    Add sweet alert and menu options

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require('ejs');
 const mongoose = require('mongoose');
+const nodemailer = require("nodemailer");
+
 
 const app = express();
+
+app.set('view engine', 'ejs');
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
+
 
 mongoose.connect('mongodb+srv://admin-felipe:1512@cluster0.hv43z.mongodb.net/paniniDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -27,11 +35,15 @@ const messageSchema = new mongoose.Schema({
 const Message = mongoose.model('Message', messageSchema);
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+  res.render("index", {firstMessage: "Fresh Food", secondMessage:"Comida feita a partir do zero" });
 });
 
 app.get("/contact", (req,res)=> {
-  res.sendFile(__dirname + "/public/contact.html")
+  res.render('contact', {firstMessage: "Contato", secondMessage:"Entre em contato conosco"});
+})
+
+app.get("/success", (req, res) => {
+  res.render("success");
 })
 
 app.post("/contact", (req, res) => {
@@ -41,11 +53,51 @@ app.post("/contact", (req, res) => {
     message: req.body.message
   })
 
-  message.save(function (err){
-    if(!err){
-      res.redirect("/contact");
+  
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({    
+    host: 'smtp.ethereal.email',
+    port: 587,
+    
+    auth: {
+      user: "dominique.gulgowski37@ethereal.email", // 
+      pass: "tmw4kMUNzgMYCv9UzY", // 
+    },
+
+  });
+
+  // send mail with defined transport object
+  let mailOptions = {
+    from: '"Panini website" <dominique.gulgowski37@ethereal.email>', // sender address
+    to: "dominique.gulgowski37@ethereal.email", // list of receivers
+    replyTo: req.body.email,
+    subject: "Panini mensagem", // Subject line
+    
+    html: `<p>Nova mensagem recebida</p>
+    <h3>Detalhes do contato</h3>
+    <ul>
+    <li>Nome: ${req.body.name}</li>
+    <li>Email: ${req.body.email}</li>
+    <li>Mensagem: ${req.body.message}</li>
+    </ul>`, 
+  };
+
+  message.save(function(err){
+    if(err){
+      console.log(err);
+    }else {
+      transporter.sendMail(mailOptions, function(err,info){
+        if(err){
+          console.log(err);
+        }else {      
+    
+          console.log("Email sent");
+          
+        }
+      }) 
     }
-  }) 
+  })
+
 
 
   
--- public/script.js
+++ public/script.js
@@ -1,8 +1,56 @@
 
 const toggleButton = document.getElementsByClassName("toggle-button")[0]
 const navbarLinks = document.getElementsByClassName("navbar-links")[0]
+const buttonSend = document.getElementById("send-message");
+const tabs = document.querySelectorAll("[data-tab-target]");
+const tabContents = document.querySelectorAll("[data-tab-content]");
+
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget);
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active');
+        })
+        tabs.forEach(tab => {
+            tab.classList.remove('active');
+        })
+        tab.classList.add('active')
+        target.classList.add('active');
+    })
+})
+
 
 toggleButton.addEventListener("click", ()=> {
     navbarLinks.classList.toggle("active")
-
 })
+
+buttonSend.addEventListener("click", ()=> {
+
+    const nome = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;    
+    
+    if(nome == "" || email == "" || message == ""){
+        swal({
+            title: "Ops!",
+            text: "Verifique os campos novamente",
+            icon: "error",
+            buttons: {
+                confirm : {text:'OK',className:'sweet-confirm'},                
+            },
+        })
+    } else {
+        swal({
+            title: "Obrigado!",
+            text: "Mensagem enviada com sucesso!",
+            icon: "success",
+            buttons: {
+                confirm : {text:'OK',className:'sweet-confirm'},                
+            },
+        }).then(function() {
+            window.location = "http://localhost:3000/contact";
+        });
+    }
+})
+
