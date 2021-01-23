5ceef3843296c03a3983a94610b6d36092541883
Felipe Chung <fchung.dev@gmail.com>
2021-01-22 20:28:54 -0600

    restaurant website files commit

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-Restaurant Website
+# Panini Website
+
+---
+
+### Table of Contents
+
+- [Description](#description)
+- [Author Info](#author-info)
+
+---
+
+## Description
+
+Restaurant website to practice skills
+
+#### Technologies
+
+- HTML
+- CSS
+- JavaScript
+- EJS
+- Node.js
+
+[Back To The Top](#panini-website)
+
+---
+
+## Author Info
+
+- Email - [fchung.dev@gmail.com](fchung.dev@gmail.com)
+- LinkedIn - [Felipe Chung](https://www.linkedin.com/in/felipe-chung-806635138/)
+
+[Back To The Top](#panini-website-website)
--- app.js
+++ app.js
@@ -1,106 +1,111 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require('ejs');
-const mongoose = require('mongoose');
+const ejs = require("ejs");
+const mongoose = require("mongoose");
 const nodemailer = require("nodemailer");
-
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-mongoose.connect('mongodb+srv://admin-felipe:1512@cluster0.hv43z.mongodb.net/paniniDB', {useNewUrlParser: true, useUnifiedTopology: true});
-
+mongoose.connect(
+  "mongodb+srv://admin-felipe:1512@cluster0.hv43z.mongodb.net/paniniDB",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 const messageSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
-  email:{
+  email: {
     type: String,
-    required: true
+    required: true,
   },
   message: {
     type: String,
-    required: true
-  }  
+    required: true,
+  },
 });
 
-const Message = mongoose.model('Message', messageSchema);
+const Message = mongoose.model("Message", messageSchema);
 
 app.get("/", (req, res) => {
-  res.render("index", {firstMessage: "Fresh Food", secondMessage:"Comida feita a partir do zero", target:"#menu" });
+  res.render("index", {
+    firstMessage: "Fresh Food",
+    secondMessage: "Comida feita a partir do zero",
+    target: "#menu",
+  });
 });
 
-app.get("/contact", (req,res)=> {
-  res.render('contact', {firstMessage: "Contato", secondMessage:"Entre em contato conosco", target:"#contact-page"});
-})
+app.get("/contact", (req, res) => {
+  res.render("contact", {
+    firstMessage: "Contato",
+    secondMessage: "Entre em contato conosco",
+    target: "#contact-page",
+  });
+});
 
 app.get("/menu", (req, res) => {
-  res.render("menu", {firstMessage: "Cardápio", secondMessage:"Veja o nosso cardápio", target:"#menu"});
-})
+  res.render("menu", {
+    firstMessage: "Cardápio",
+    secondMessage: "Veja o nosso cardápio",
+    target: "#menu",
+  });
+});
 
 app.post("/contact", (req, res) => {
-  const message = new Message ({
+  const message = new Message({
     name: req.body.name,
     email: req.body.email,
-    message: req.body.message
-  })
+    message: req.body.message,
+  });
 
-  
   // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({    
-    host: 'smtp.ethereal.email',
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
     port: 587,
-    
-    auth: {
-      user: "julie.weimann@ethereal.email", // 
-      pass: "aS2Gzr3ndWcFTMVQnY", // 
-    },
 
+    auth: {
+      user: process.env.EMAIL, // email from .env
+      pass: process.env.PASSWORD, // password from .env
+    },
   });
 
   // send mail with defined transport object
   let mailOptions = {
-    from: '"Panini website" <julie.weimann@ethereal.email>', // sender address
-    to: "julie.weimann@ethereal.email", // list of receivers
+    from: '"Panini website" <' + process.env.EMAIL + ">", // sender address
+    to: "receiveremail@email.com", // list of receivers
     replyTo: req.body.email,
     subject: "Panini mensagem", // Subject line
-    
+
     html: `<p>Nova mensagem recebida</p>
     <h3>Detalhes do contato</h3>
     <ul>
     <li>Nome: ${req.body.name}</li>
     <li>Email: ${req.body.email}</li>
     <li>Mensagem: ${req.body.message}</li>
-    </ul>`, 
+    </ul>`,
   };
 
-  message.save(function(err){
-    if(err){
-      console.log(err);      
-    }else {
-      transporter.sendMail(mailOptions, function(err,info){
-        if(err){
+  message.save(function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      transporter.sendMail(mailOptions, function (err, info) {
+        if (err) {
           console.log(err);
-        }else {      
-          
+        } else {
           console.log("Email sent");
-          res.redirect("/contact")
+          res.redirect("/contact");
         }
-      }) 
+      });
     }
-  })
-
-
-
-  
+  });
 });
 
 let port = process.env.PORT;
